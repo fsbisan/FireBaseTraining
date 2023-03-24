1fc89410cdc158ee0e5f073db481080130f723b0
Alexander Makarov <fsbisan@mail.ru>
2023-03-24 18:35:46 +0500

    Initial Commit

---
+++ FireBaseTraining/FireBaseTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseTrainingApp.swift
+//  FireBaseTraining
+//
+//  Created by Александр Макаров on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
